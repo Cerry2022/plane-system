0b9a3740f2b9920bd4cd3223d83e30248ae483b0
Cerry2022 <1720057865@QQ.com>
2024-06-05 15:28:37 +0800

    更新ui

--- src/core/ui/first/LoginJpanel.java
+++ src/core/ui/first/LoginJpanel.java
@@ -3,6 +3,10 @@ package core.ui.first;
 import com.formdev.flatlaf.FlatLightLaf;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class LoginJpanel {
     public JPanel LoginJpanel;
@@ -12,5 +16,31 @@ public class LoginJpanel {
     private JLabel passwordLable;
     private JButton loginButton;
     private JButton cancelLoginButton;
+    private JLabel registerLable;
 
+    public LoginJpanel() {
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        cancelLoginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        registerLable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+            }
+        });
+    }
 }
